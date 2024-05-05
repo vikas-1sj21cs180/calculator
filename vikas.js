bed0bdf8cfd5906e6a98c83961408455bc0659a1
vikas 1sj21cs180 <vv9077022@gmail.com>
2024-05-05 17:00:58 +0530

    vikas first own project

---
+++ vikas.js
@@ -0,0 +1,26 @@
+(function(){
+    let screen=document.querySelector('.screen');
+    let buttons=document.querySelectorAll('.btn');
+    let equal=document.querySelector('.btn-equal');
+    let clear=document.querySelector('.btn-clear');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e){
+            let value= e.target.innerText;
+            screen.value += value;
+        })
+    })
+    
+    clear.addEventListener('click',function(e){
+        screen.value=''
+    })
+
+    equal.addEventListener('click',function(e){
+        if(screen.value===''){
+            screen.value='please enter';
+        }else{
+            let ans=eval(screen.value);
+            screen.value=ans;
+        }
+    })
+})();
